6552108c5424d986b7112cb0d226f603b27c77e6
1nt3gr4 <vadem.gati@gmail.com>
2017-06-05 10:25:40 +0300

    Refresh.

---
+++ ShopsDataProcessing/ShopsDataProcessing/Processing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopsDataProcessing
+{
+    public class Shop
+    {
+        public int ShopCode { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Price
+    {
+        public int ShopCode { get; set; }
+        public int GoodCode { get; set; }
+        public int GoodPrice { get; set; }
+        public int Discount { get; set; }
+    }
+
+    public class Good
+    {
+        public int GoodCode { get; set; }
+        public string GoodName { get; set; }
+        public string Category { get; set; }
+    }
+
+    class Processing
+    {
+        public List<Shop> Shops { get; set; }
+        public List<Price> Prices { get; set; }
+        public List<Good> Goods { get; set; }
+
+
+    }
+}
